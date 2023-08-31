a442cb03ac3f7fd69eae0fa27d8f3bb411051c49
Илья Василевский <vasilevskijila047@gmail.com>
2023-08-31 21:30:17 +0300

    Version 1.1

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    FLASK_APP = os.getenv('FLASK_APP', default='yacut')
+    FLASK_ENV = os.getenv('FLASK_ENV', default='development')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='8f42a73054b1749f8f58848be5e6502c')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
